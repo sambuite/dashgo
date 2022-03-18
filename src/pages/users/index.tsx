95e0ca73195104fe62cba9cfb720a2ec0201a32c
sambuite <murilosambuite@gmail.com>
2022-03-18 02:48:17 -0300

    feat: create users page with a table of users

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,92 @@
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  Heading,
+  Icon,
+  Table,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+} from '@chakra-ui/react';
+import { RiAddLine, RiEditLine } from 'react-icons/ri';
+import { Header } from '../../components/Header';
+import { Sidebar } from '../../components/Sidebar';
+
+export default function UserList() {
+  return (
+    <Box>
+      <Header />
+
+      <Flex w='100%' my='6' maxWidth={1480} mx='auto' px='6'>
+        <Sidebar />
+
+        <Box flex='1' borderRadius={8} bg='gray.800' p='8'>
+          <Flex mb='8' justify='space-between' align='center'>
+            <Heading size='lg' fontWeight='normal'>
+              Usuários
+            </Heading>
+
+            <Button
+              as='a'
+              size='sm'
+              fontSize='sm'
+              colorScheme='pink'
+              leftIcon={<Icon as={RiAddLine} fontSize='20' />}
+            >
+              Criar novo
+            </Button>
+          </Flex>
+
+          <Table colorScheme='whiteAlpha'>
+            <Thead>
+              <Tr>
+                <Th px='6' color='gray.300' width='8'>
+                  <Checkbox colorScheme='pink' />
+                </Th>
+                <Th>Usuário</Th>
+                <Th>Data de cadastro</Th>
+                <Th w='8' />
+              </Tr>
+            </Thead>
+            <Tbody>
+              {Array.from({ length: 3 }).map((_, index) => (
+                <Tr key={index}>
+                  <Td px='6'>
+                    <Checkbox colorScheme='pink' />
+                  </Td>
+                  <Td>
+                    <Box>
+                      <Text fontWeight='bold'>Murilo Sambuite</Text>
+                      <Text fontSize='sm' color='gray.300'>
+                        Murilo Sambuite
+                      </Text>
+                    </Box>
+                  </Td>
+                  <Td>18 de Março, 2022</Td>
+                  <Td>
+                    {' '}
+                    <Button
+                      as='a'
+                      size='sm'
+                      fontSize='sm'
+                      colorScheme='purple'
+                      color='white'
+                      leftIcon={<Icon as={RiEditLine} fontSize='16' />}
+                    >
+                      Editar
+                    </Button>
+                  </Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        </Box>
+      </Flex>
+    </Box>
+  );
+}
