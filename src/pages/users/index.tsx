e6d0422bfe686f0ede202a1d5fbd41bd279df86f
sambuite <murilosambuite@gmail.com>
2022-03-18 02:59:11 -0300

    feat: create Pagination component and add to users table

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -15,6 +15,7 @@ import {
 } from '@chakra-ui/react';
 import { RiAddLine, RiEditLine } from 'react-icons/ri';
 import { Header } from '../../components/Header';
+import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
 
 export default function UserList() {
@@ -85,6 +86,7 @@ export default function UserList() {
               ))}
             </Tbody>
           </Table>
+          <Pagination />
         </Box>
       </Flex>
     </Box>
