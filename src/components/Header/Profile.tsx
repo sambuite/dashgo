6a65fbf010509385945cd9f61a261fd358750e6f
sambuite <murilosambuite@gmail.com>
2022-03-19 08:57:21 -0300

    feat: add responsiveness to Header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,15 +1,20 @@
 import { Avatar, Box, Flex, Text } from '@chakra-ui/react';
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData }: ProfileProps) {
   return (
     <Flex align='center'>
-      <Box mr='4' textAlign='right'>
-        <Text>Murilo Sambuite</Text>
-        <Text color='gray.300' fontSize='small'>
-          murilosambuite@gmail.com
-        </Text>
-      </Box>
-
+      {showProfileData && (
+        <Box mr='4' textAlign='right'>
+          <Text>Murilo Sambuite</Text>
+          <Text color='gray.300' fontSize='small'>
+            murilosambuite@gmail.com
+          </Text>
+        </Box>
+      )}
       <Avatar
         size='md'
         name='Murilo Sambuite'
