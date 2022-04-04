c705bde1bcae4d9c762b92b89c0acba96a4fc496
sambuite <murilosambuite@gmail.com>
2022-04-04 20:25:04 -0300

    feat: add react-query and use it to list users

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -5,6 +5,7 @@ import {
   Flex,
   Heading,
   Icon,
+  Spinner,
   Table,
   Tbody,
   Td,
@@ -16,11 +17,38 @@ import {
 } from '@chakra-ui/react';
 import Link from 'next/link';
 import { RiAddLine, RiEditLine } from 'react-icons/ri';
+import { useQuery } from 'react-query';
 import { Header } from '../../components/Header';
 import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
 
 export default function UserList() {
+  const { data, isLoading, error } = useQuery(
+    'users',
+    async () => {
+      const response = await fetch('http://localhost:3000/api/users');
+      const data = await response.json();
+
+      const users = data.users.map((user) => {
+        return {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+            day: '2-digit',
+            month: 'long',
+            year: 'numeric',
+          }),
+        };
+      });
+
+      return users;
+    },
+    {
+      staleTime: 1000 * 5,
+    },
+  );
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
@@ -52,57 +80,69 @@ export default function UserList() {
             </Link>
           </Flex>
 
-          <Table colorScheme='whiteAlpha'>
-            <Thead>
-              <Tr>
-                <Th px={['4', '4', '6']} color='gray.300' width='8'>
-                  <Checkbox colorScheme='pink' />
-                </Th>
-                <Th>Usuário</Th>
-                {isWideVersion && (
-                  <>
-                    <Th>Data de cadastro</Th>
-                    <Th w='8' />
-                  </>
-                )}
-              </Tr>
-            </Thead>
-            <Tbody>
-              {Array.from({ length: 3 }).map((_, index) => (
-                <Tr key={index}>
-                  <Td px={['4', '4', '6']}>
-                    <Checkbox colorScheme='pink' />
-                  </Td>
-                  <Td>
-                    <Box>
-                      <Text fontWeight='bold'>Murilo Sambuite</Text>
-                      <Text fontSize='sm' color='gray.300'>
-                        murilosambuite@gmail.com
-                      </Text>
-                    </Box>
-                  </Td>
-                  {isWideVersion && (
-                    <>
-                      <Td>18 de Março, 2022</Td>
-                      <Td>
-                        <Button
-                          as='a'
-                          size='sm'
-                          fontSize='sm'
-                          colorScheme='purple'
-                          color='white'
-                          leftIcon={<Icon as={RiEditLine} fontSize='16' />}
-                        >
-                          Editar
-                        </Button>
+          {isLoading ? (
+            <Flex justify='center'>
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify='center'>
+              <Text>Falha ao obter dados do usuário</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme='whiteAlpha'>
+                <Thead>
+                  <Tr>
+                    <Th px={['4', '4', '6']} color='gray.300' width='8'>
+                      <Checkbox colorScheme='pink' />
+                    </Th>
+                    <Th>Usuário</Th>
+                    {isWideVersion && (
+                      <>
+                        <Th>Data de cadastro</Th>
+                        <Th w='8' />
+                      </>
+                    )}
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {data.map((user) => (
+                    <Tr key={user.id}>
+                      <Td px={['4', '4', '6']}>
+                        <Checkbox colorScheme='pink' />
                       </Td>
-                    </>
-                  )}
-                </Tr>
-              ))}
-            </Tbody>
-          </Table>
-          <Pagination />
+                      <Td>
+                        <Box>
+                          <Text fontWeight='bold'>{user.name}</Text>
+                          <Text fontSize='sm' color='gray.300'>
+                            {user.email}
+                          </Text>
+                        </Box>
+                      </Td>
+                      {isWideVersion && (
+                        <>
+                          <Td>{user.createdAt}</Td>
+                          <Td>
+                            <Button
+                              as='a'
+                              size='sm'
+                              fontSize='sm'
+                              colorScheme='purple'
+                              color='white'
+                              leftIcon={<Icon as={RiEditLine} fontSize='16' />}
+                            >
+                              Editar
+                            </Button>
+                          </Td>
+                        </>
+                      )}
+                    </Tr>
+                  ))}
+                </Tbody>
+              </Table>
+              <Pagination />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
